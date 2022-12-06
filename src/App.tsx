d3e42a7bc92fefafdf1586eba734704d98579c06
Sanni_Damilola <114070063+Sanni-Damilola@users.noreply.github.com>
2022-12-06 17:22:07 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Allroutes from "./Components/Routes/Allroutes";
+
+function App() {
+  return (
+    <div>
+      <Allroutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Explore/Explore.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Explore = () => {
+  return <div>On going</div>;
+};
+
+export default Explore;
